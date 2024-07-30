7b5f45c7fb914326b50797bc34008939183bc660
Andreas Riedmüller <andreas@simplease.at>
2024-07-30 11:27:52 +0200

    configure eslint-plugin-simple-import-sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,9 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "eslint-plugin-simple-import-sort"],
   rules: {
+    "simple-import-sort/imports": ["error"],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
