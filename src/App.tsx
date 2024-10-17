d2df16b0fe1be81bb620d0ddcd80f64ad8d7e1b8
Andreas Riedmüller <andreas@simplease.at>
2024-10-17 15:46:30 +0200

    feat: install and config @sys42

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 
+import { Button, classCard, Stack } from "@sys42/ui";
+import { cn } from "@sys42/utils";
 import { useState } from "react";
 
 import viteLogo from "/vite.svg";
@@ -10,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Stack>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -20,18 +22,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <Stack className={cn(classCard, "card")}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </Stack>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </Stack>
   );
 }
 
