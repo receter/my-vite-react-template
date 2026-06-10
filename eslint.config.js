c17a4cf099e3734c8ef1be234c4329a770bc2289
Andreas Riedmüller <andreas@simplease.at>
2026-06-10 16:44:25 +0200

    chore: upgrade to vite 8

--- eslint.config.js
+++ eslint.config.js
@@ -13,10 +13,12 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+    },
     plugins: { "simple-import-sort": simpleImportSort },
     rules: { "simple-import-sort/imports": ["error"] },
   },
--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+});
